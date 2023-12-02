6824ffb4a16a8e61b1f57db6c7954965e50de574
kells\KellsPing <kellsping@gmail.com>
2023-12-02 11:48:42 +0800

    See all members.

--- app/src/main/java/com/example/mychatroom/MainActivity.java
+++ app/src/main/java/com/example/mychatroom/MainActivity.java
@@ -2,21 +2,61 @@ package com.example.mychatroom;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.example.mychatroom.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "kells";
+
+    ActivityMainBinding binding;
+    DatabaseReference databaseReference;
+    UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        userAdapter = new UserAdapter(this);
+        binding.recycler.setAdapter(userAdapter);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userAdapter.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Log.d(TAG, "Key:" + dataSnapshot.getKey());
+                    String uid = dataSnapshot.getKey();
+                    if (!uid.equals(FirebaseAuth.getInstance().getUid())){
+                        UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                        userAdapter.add(userModel);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
